298a504dfeac2d3f939515ff385b6e492cb4fedc
Sam Selikoff <sam.selikoff@gmail.com>
2015-07-29 11:40:52 -0400

    [#207] Serializer: Format model attribute keys

--- addon/utils/inflector.js
+++ addon/utils/inflector.js
@@ -3,3 +3,4 @@ import Ember from 'ember';
 export var singularize = Ember.String.singularize;
 export var pluralize = Ember.String.pluralize;
 export var capitalize = Ember.String.capitalize;
+export var camelize = Ember.String.camelize;
